94c60dc7bd9c0f767b7d31d734a38113bfa434f4
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-15 03:55:34 +0300

    Add the comment route

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,8 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from ..requests import get_quotes
 from .forms import CommentsForm
-from ..models import Comments
+from ..models import Comments, Blog, User
+from flask_login import login_required
 
 @main.route('/')
 def index():
@@ -16,16 +17,16 @@ def index():
     title = 'Quote of the hour.'
     return render_template('index.html', title = title, feature= featured_quotes)
 
-# @main.route('/user/comment/new/<int:id>', methods =["GET", "POST"])
-# @login_required
-# def comment(id):
-#     form = CommentsForm()
-#     comments = Comments.query.filter_by(pitch_id = id).all()
-#     blog = Blog.query.filter_by(id = id).first()
+@main.route('/user/comment/new/<int:id>', methods =["GET", "POST"])
+@login_required
+def comment(id):
+    form = CommentsForm()
+    comments = Comments.query.filter_by(blog_id = id).all()
+    blog = Blog.query.filter_by(id = id).first()
 
-#     if form.validate_on_submit():
-#         comment_submitted = form.comment.data
-#         new_comment = Comments(comment= comment_submitted, commenter = current_user, comments = blog )
-#         new_comment.save_comment()
+    if form.validate_on_submit():
+        comment_submitted = form.comment.data
+        new_comment = Comments(comment= comment_submitted, commenter = current_user, comments = blog )
+        new_comment.save_comment()
 
-#     return render_template('comments.html', comment_form = form, comments = comments, blog = blog)  
+    return render_template('comments.html', comment_form = form, comments = comments, blog = blog)  
---
+++ tests/test_blog.py
@@ -0,0 +1,26 @@
+import unittest
+from app.models import Blog, User
+from app import db
+
+class TestBlog(unittest.TestCase):
+    def setUp(self):
+        self.user_ruweydha = User(username='ruweydha', password ='potato', email = 'ruweydhaabdinoor@gmail.com')
+        self.new_blog = Blog(title= 'Technology', content = 'Software development',user_id = self.user_ruweydha.id)
+
+    def tearDown(self):
+        Blog.query.delete() 
+        User.query.delete() 
+
+    def test_check_instance_variables(self) :
+        self.assertEquals(self.new_blog.title, 'Technology') 
+        self.assertEquals(self.new_blog.content, 'Software development')  
+        self.assertEquals(self.new_blog.user_id, self.user_ruweydha.id)  
+
+    def test_save_blog(self):
+        self.new_blog.save_blog()  
+        self.assertTrue(len(Blog.query.all())>0)  
+
+    def test_get_pitch_by_id(self):
+        self.new_blog.save_blog() 
+        got_blog = Blog.get_blog(self.user_ruweydha.id) 
+        self.assertTrue(len(got_blog)==1) 
