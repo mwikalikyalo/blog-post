d3ff7a1387d76b53ce319e3b2918bcf6eeca0a44
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-15 23:01:24 +0300

    Add the new database

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app
 from flask_script import Manager,Server
 from app import create_app,db
-from app.models import User
+from app.models import Role, User
 
 # Creating app instance
 app = create_app('development')
@@ -11,7 +11,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db, User = User, Role= Role)
 
 @manager.command
 def test():
