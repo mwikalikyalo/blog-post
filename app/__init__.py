5dba79e8338ad1672fcde5220d25a746007b1ffc
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2022-02-16 05:41:04 +0300

    Add the mail username and password exports

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_email import EmailMessage
 
+#........
+mail = EmailMessage()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-export SECRET_KEY='Quota'
+export SECRET_KEY= Quota
+export MAIL_USERNAME= winniemwikali07@gmail.com
+export MAIL_PASSWORD= Ky1ky1@ermy
 
 python3.8 manage.py server
